07ee92c1fcd30eec8c66fe063ba99cda0b2b0c5c
zhigongzhang <zhigongzhang@zhigongzhang-PC>
2018-12-28 18:01:47 +0800

    优化授权关系

--- src/test/java/com/test/TestJson.java
+++ src/test/java/com/test/TestJson.java
@@ -1,9 +1,11 @@
 package com.test;
 
+import org.apache.commons.validator.routines.RegexValidator;
 import org.junit.Test;
 
 import com.alibaba.fastjson.JSONObject;
 import com.bugbycode.module.user.User;
+import com.util.reg.RegexUtil;
 
 public class TestJson {
 
@@ -13,5 +15,11 @@ public class TestJson {
 		json.put("name", "zhangsan");
 		User user = JSONObject.toJavaObject(json, User.class);
 		System.out.println(user.getName());
+	}
+	
+	@Test
+	public void testValidate() {
+		boolean check = RegexUtil.check(RegexUtil.EMAIL_REGEX, null);
+		System.out.println(check);
 	}
 }
