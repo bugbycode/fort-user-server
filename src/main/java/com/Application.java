a8ff695ca45a928bdb1af498fb4fa14744cd463d
zhigongzhang <zhigongzhang@192.168.138.1>
2019-03-15 12:49:48 +0800

    首次创建

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -2,11 +2,18 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
